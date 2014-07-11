513036471d265641c800d93d25b1bd8309773679
Tommmy <TommmyLee@qq.com>
2014-07-11 14:46:44 +0800

    Revert "学习项目"
    This reverts commit 97e96a77df55480fd30b70f69b0bbe9d8b9c8252.

--- middlewares/github_strategy.js
+++
@@ -1,7 +0,0 @@
-var Models = require('../models');
-var User = Models.User;
-
-module.exports = function (accessToken, refreshToken, profile, done) {
-  profile.accessToken = accessToken;
-  done(null, profile);
-};
